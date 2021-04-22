bce61eedb69344e0db706b46cbcf5452f8dc49bc
Yufan Yuan <yufan.yuan@evocolabs.com>
2021-04-22 20:22:44 +0800

    Completed the presentation

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import './App.css';
-import { RevealJS } from '@gregcello/revealjs-react'
-import { HighlightPlugin } from '@gregcello/revealjs-react'
-import Welcome from './topics/welcome/welcome';
-import GitSegment from './topics/git/git';
-import CodingStyle from './topics/codingStyle/codingStyle';
-import { useEffect } from 'react';
+import './App.css'
+import { RevealJS, HighlightPlugin } from '@gregcello/revealjs-react'
 
-function App() {
+import Welcome from './topics/welcome/welcome'
+import GitSegment from './topics/git/git'
+import CodingStyle from './topics/codingStyle/codingStyle'
+import { useEffect } from 'react'
+import CodeReview from './topics/codeReview/codeReview'
+
+function App () {
   useEffect(() => {
 
   }, [])
   return (
-    <div className="App" >
-    {/* <div>你好！</div> */}
-        <RevealJS
-          width={960}
-          height={720}
-          
-          plugins={[ HighlightPlugin ]}
-          >
-          <Welcome />
-          <CodingStyle />
-          <GitSegment />
-        </RevealJS>
+    <div className='App'>
+      {/* <div>你好！</div> */}
+      <RevealJS
+        width={1280}
+        height={960}
+        plugins={[HighlightPlugin]}
+      >
+        <Welcome />
+        <CodingStyle />
+        <GitSegment />
+        <CodeReview />
+      </RevealJS>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/topics/codeReview/codeReview.js
@@ -0,0 +1,13 @@
+import { Slide, H1, Image } from '@gregcello/revealjs-react'
+import React from 'react'
+
+const CodeReview = () => {
+  return (
+    <Slide>
+      <H1>Code Review</H1>
+      <Image src='tooLongCodeReview.jpg' />
+    </Slide>
+  )
+}
+
+export default CodeReview
